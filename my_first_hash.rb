aa4c5452008dff3cf21159ea65652bcc9a496c3b
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 00:55:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,11 +29,11 @@ def adding
   #               }
   # add 2 muskets to the shipping_manifest hash below
   ship_manifest = shipping_manifest
-  shipping_manifest["muskets"] = 2
+  ship_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powder"] = 4
+  ship_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
