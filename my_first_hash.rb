e47f08b5ad3af0df1f0ad22e3c022e75d80d2ef1
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 00:44:33 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,8 @@ end
 
 
 def shipping_manifest
-  the_manifest = { "whale bone corsets" => 5, 2 "porcelain vases" => 2, "oil paintings" => 3 }
+  # the_manifest = { "whale bone corsets" => 5, 2 "porcelain vases" => 2, "oil paintings" => 3 }
+  the_manifest = retrieval
 end
 
 def retrieval
