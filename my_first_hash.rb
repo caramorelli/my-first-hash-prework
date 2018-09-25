207b944c98b3e3b272b470c5f0cf005e43d4e768
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 00:50:29 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,11 +9,11 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+  # shipping_manifest = {
+  #                 "whale bone corsets" => 5, 
+  #                 "porcelain vases" => 2, 
+  #                 "oil paintings" => 3
+  #               }
   shipping_manifest["oil paintings"]
 
 end
